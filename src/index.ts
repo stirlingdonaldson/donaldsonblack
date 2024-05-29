9c919717c4ad8fbec533ba48cf65a5657c21af32
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-29 12:42:34 +1200

    added text flip quirk on index.html, that flips the hero a items along their x axis

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,13 @@ const server = Bun.serve({
     const url = new URL(req.url);
     console.log(url.pathname);
     if (url.pathname === "/") return new Response(Bun.file("src/index.html"));
-    if (url.pathname === "/styles.css") return new Response(Bun.file("src/style.css"));
+    if (url.pathname === "/index.css") return new Response(Bun.file("src/index.css"));
     if (url.pathname === "/about-us.html") return new Response(Bun.file("src/about-us.html"));
+    if (url.pathname === "/about-us.css") return new Response(Bun.file("src/about-us.css"));
+    if (url.pathname === "/info.html") return new Response(Bun.file("src/info.html"));
+    if (url.pathname === "/info.css") return new Response(Bun.file("src/info.css"));
+    if (url.pathname === "/work.html") return new Response(Bun.file("src/work.html"));
+    if (url.pathname === "/work.css") return new Response(Bun.file("src/work.css"));
   },
 });
 
