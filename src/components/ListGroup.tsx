48bc8ad0924776ea3128a5dbafffadaf95fdc2ee
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 16:42:58 +1000

    made list dynamic using array and mapping it

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
 function ListGroup() {
-  return (
-    <>  
+    const items = [
+        'New York',
+        'Auckland',
+        'Tokyo',
+        'Melbourne',
+        'Sydney'
+    ];
+
+    
+    
+    return (
+        <>  
     <h1>List</h1>
     
     <ul className="list-group">
-      <li className="list-group-item active" aria-current="true">An active item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+        { items.map(item => <li>{item}</li>) /* This will return an array of JSX elements, where item is being turned into li elements, curly braces render code dynamically */  }     
     </ul>
     </>
   );
