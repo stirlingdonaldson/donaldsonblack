f30fd66427f0d619e3408c5e8de8909809a9bb4d
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 16:37:05 +1000

    wrapped list group content in fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,17 @@
-function ListGroup () {
-    return <ul className="list-group">
-    <li className="list-group-item active" aria-current="true">An active item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul>
+function ListGroup() {
+  return (
+    <>  
+    <h1>List</h1>
+    
+    <ul className="list-group">
+      <li className="list-group-item active" aria-current="true">An active item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
+    </>
+  );
 }
 
 export default ListGroup;
