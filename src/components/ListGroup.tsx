0dbbf28861255cba678d2fffdfd4610f43439896
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 17:02:14 +1000

    changed ? ternary operator in code for &&

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -13,7 +13,7 @@ function ListGroup() {
     return (
         <>
             <h1>List</h1>
-            {items.length === 0 ? <p>No item found</p> : null}
+            {items.length === 0 && <p>No item found</p>}
         </>
     );
     
