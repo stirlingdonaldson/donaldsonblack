1015f35cac62caa0899e2e3e23369982ba9af2b5
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 17:06:48 +1000

    added className="list-group-item"

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -8,25 +8,27 @@ function ListGroup() {
         'Melbourne',
         'Sydney'
     ];
-    items = [];
 
     return (
         <>
             <h1>List</h1>
             {items.length === 0 && <p>No item found</p>}
+
+            <ul className="list-group">
+                { items.map(item => <li className="list-group-item" key={item}> {item}</li>) 
+
+
+                /* This will return an array of JSX elements, where item is being turned into li elements
+                curly braces render code dynamically,
+                key element should be used to elements can be added or removed dynamically */  }     
+            </ul>
         </>
     );
     
     return (
-        <>  
+    <>  
     <h1>List</h1>
     
-    <ul className="list-group">
-        { items.map(item => <li key={item}> {item}</li>) 
-        /* This will return an array of JSX elements, where item is being turned into li elements
-        curly braces render code dynamically,
-        key element should be used to elements can be added or removed dynamically */  }     
-    </ul>
     </>
   );
 }
