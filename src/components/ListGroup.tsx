168e4bfedce455cc73c67f3ff166d71188391600
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 16:54:54 +1000

    added ternary operator for an if statement

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,29 @@
 function ListGroup() {
-    const items = [
+
+
+    let items = [
         'New York',
         'Auckland',
         'Tokyo',
         'Melbourne',
         'Sydney'
     ];
+    items = [];
 
-    
+   
+    return (
+        <>
+            <h1>List</h1>
+            {items.length === 0 ? <p>No item found</p> : null} {/* if item is empty, return <p> else do nothing */}
+        </>
+    );
     
     return (
         <>  
     <h1>List</h1>
     
     <ul className="list-group">
-        { items.map(item => <li> key={item}{item}</li>) 
+        { items.map(item => <li key={item}> {item}</li>) 
         /* This will return an array of JSX elements, where item is being turned into li elements
         curly braces render code dynamically,
         key element should be used to elements can be added or removed dynamically */  }     
