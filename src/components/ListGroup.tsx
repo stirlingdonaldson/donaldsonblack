c7f7b2687a857ff7a505c7ef98d6faf9171b65d6
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 16:34:04 +1000

    added bootstrap list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,11 @@
 function ListGroup () {
-    return <h1>ListGroup</h1>
+    return <ul className="list-group">
+    <li className="list-group-item active" aria-current="true">An active item</li>
+    <li className="list-group-item">A second item</li>
+    <li className="list-group-item">A third item</li>
+    <li className="list-group-item">A fourth item</li>
+    <li className="list-group-item">And a fifth one</li>
+  </ul>
 }
 
 export default ListGroup;
