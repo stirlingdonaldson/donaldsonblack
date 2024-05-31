acc6a75def80674730608f4452f3c26a7cbe555a
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 16:27:44 +1000

    cleared app.css

--- src/Message.tsx
+++ src/Message.tsx
@@ -4,8 +4,22 @@ function Message() {
 
     // JSX: JavaScript XML
     // go to babeljs.io/repl to see how JSX is converted to JavaScript
-    return <h1>donaldsonblack</h1>;
+    // an expression is any code that produces a value. Any expression can be added in the curly braces
+
+    const name = 'harrison';
+    return <h1>donaldsonblack {name}</h1>;
+
+    // if statements can be used (code is unreachable becase something is already being returned)
+    if (name)
+        return <h1>donaldsonblack {name}</h1>;
+    else
+        return <h1>donaldsonblacker</h1>;
+
 }
+
 
 // to use component, export it as a default object from this module
 export default Message;
+
+
+
