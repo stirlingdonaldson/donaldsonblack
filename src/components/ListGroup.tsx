96e7257ae84684f280feaba184248a357142f62c
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 16:46:25 +1000

    added element key, set to item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -14,7 +14,10 @@ function ListGroup() {
     <h1>List</h1>
     
     <ul className="list-group">
-        { items.map(item => <li>{item}</li>) /* This will return an array of JSX elements, where item is being turned into li elements, curly braces render code dynamically */  }     
+        { items.map(item => <li> key={item}{item}</li>) 
+        /* This will return an array of JSX elements, where item is being turned into li elements
+        curly braces render code dynamically,
+        key element should be used to elements can be added or removed dynamically */  }     
     </ul>
     </>
   );
