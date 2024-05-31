fa5db984253e9ca01914590edd874316cc6c06b8
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 16:08:02 +1000

    deleted everything in App.tsx

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing- always use PascalCasing for React components and props
+function Message() {
+
+    // JSX: JavaScript XML
+    // go to babeljs.io/repl to see how JSX is converted to JavaScript
+    return <h1>donaldsonblack</h1>;
+}
+
+// to use component, export it as a default object from this module
+export default Message;
