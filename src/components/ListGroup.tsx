19e89b41185a69fd3712927dd7a9521b62f69b55
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-05-31 17:10:34 +1000

    added index value

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -15,11 +15,11 @@ function ListGroup() {
             {items.length === 0 && <p>No item found</p>}
 
             <ul className="list-group">
-                { items.map(item => 
+                { items.map((item, index) => 
                     <li 
                         className="list-group-item" 
                         key={item} 
-                        onClick={() => console.log(item)}> 
+                        onClick={() => console.log(item, index)}> 
                         
                         {item}
                     </li>) 
