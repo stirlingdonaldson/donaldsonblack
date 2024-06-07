cd615556d2841b8aa43ec9a7a409d30301750161
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-06-07 22:09:09 +1000

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ export default function Home() {
   return (
     <main className=''>
       <div className='flex items-center justify-center h-screen bg-gray-400 '>
-        <div className="container mx-auto bg-gray-200  max-w-xxl h-screen flex flex-col justify-between ">
+        <div className="container mx-auto bg-gray-200  max-w-xxl h-screen flex flex-col ">
         {/*  className=" center flex min-w-lg max-w-xl min-h-screen flex-col items-center justify-between p-24 bg-gray-500 "  */}
 
           <Header />
 
-          <h1>Home</h1>
+          <h1 className='m-10'>Home</h1>
+
         </div>
       </div>
     </main>
