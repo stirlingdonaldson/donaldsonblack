fe1262ea6c625fbe3d5e1185328b3f686da96b39
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-06-07 21:57:39 +1000

    formatted site with container and basic navbar

---
+++ src/app/header.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+  return (
+    <div className="h-20 border-4 border-b-gray-400 flex flex-row-reverse items-center m-2 p-2">
+            <h3 className="m-2">About</h3>
+            <h3 className="m-2" >Info</h3>
+    </div>
+    
+  );
+}
