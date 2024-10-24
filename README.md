ff70a5c081dffd09eff91e755803133ea1a7a7a7
Harrison Black <harrison@Mac.modem>
2024-10-24 20:38:29 +1100

    Deno Fresh

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
+
 # DonaldsonBlack
 
 ![](https://skillicons.dev/icons?i=deno,ts,tailwind,neovim,ps,pr)
 
 > Source code of photography portfolio of Stirling Donaldson and Harrison Black
+
+### Usage
+Make sure to install deno
+
+Then start the project:
+
+```bash
+deno task start
+
+```
+
+This will also watch files and realod on save
--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+import "$std/dotenv/load.ts";
+
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
+
+await start(manifest, config);
